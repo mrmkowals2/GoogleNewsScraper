c7d02fcc1a3cbfc480ebe9ca35495bd434ae8ba6
Mike <mkowals2@gmail.com>
2018-04-07 17:50:21 -0400

    Edited scraper to write output to txt file

--- GoogleNewsScraper.py
+++ GoogleNewsScraper.py
@@ -1,5 +1,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import webbrowser
 
 class Scraper:
     def __init__(self,site):
@@ -10,12 +11,30 @@ class Scraper:
         html = r.read()
         parser = "html.parser"
         sp = BeautifulSoup(html,parser)
+
+        file = open("D:\Projects\GoogleNewsScraper\GNScraperOut.txt","r+")
+        file.truncate() #clears contents of the output file before updating
+        file = file.close()
+
+        file = open("D:\Projects\GoogleNewsScraper\GNScraperOut.txt","a+")
         for tag in sp.find_all("a"):
             url = tag.get("href")
             if url is None:
                 continue
             if "html" in url:
-                print("\n" + url)
+                file.write(url + "\r\n")
+        file = file.close()
 
 news = "https://news.google.com/"
 Scraper(news).scrape()
+
+lines_seen = set()
+outfile = open("D:\Projects\GoogleNewsScraper\GNScraperDeDuped.txt","w")
+for line in open("D:\Projects\GoogleNewsScraper\GNScraperOut.txt","r"):
+    if line != "\n":
+        if line not in lines_seen:
+            outfile.write(line + "\n")
+            lines_seen.add(line)
+outfile.close()
+
+webbrowser.open("D:\Projects\GoogleNewsScraper\GNScraperDeDuped.txt")
